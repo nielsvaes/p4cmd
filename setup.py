3b4cd05d2d75a2cd46721d4bce6dcdae3bf39392
Niels Vaes <niels.vaes@embark-studios.com>
2021-11-26 14:21:18 +0100

    setting up for pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="p4cmd",
+    version="1.0.0",
+    author="Niels Vaes",
+    license='MIT',
+    author_email="nielsvaes@gmail.com",
+    description="Simple P4 python module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nielsvaes/p4cmd",
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+    ]
+)
