c0b24a2d6fab8d5378c872f39b9442181ea04f3c
Niels Vaes <nielsvaes@gmail.com>
2023-06-01 15:55:01 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ data_files_to_include = ["*.png"]
 
 setuptools.setup(
     name="p4cmd",
-    version="2.0.3",
+    version="2.0.4",
     author="Niels Vaes",
     license='MIT',
     author_email="nielsvaes@gmail.com",
