0a02498889c932718308a9bf7f0c766c391ffedb
Niels Vaes <nielsvaes@gmail.com>
2023-08-30 17:26:52 +0200

    version bump
    whaddaya know, there was more fuckery afoot than I thought 5 minutes ago

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ data_files_to_include = ["*.png"]
 
 setuptools.setup(
     name="p4cmd",
-    version="2.3.3",
+    version="2.3.4",
     author="Niels Vaes",
     license='MIT',
     author_email="nielsvaes@gmail.com",
